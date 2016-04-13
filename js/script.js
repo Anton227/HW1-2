ab4420f0d765f3343e291187d4a7978f818184db
Anton Taborovsky <taborovsky@ukr.net>
2016-04-13 23:41:53 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,29 @@
+alert('hello');
+var arr = fillArray();
+var userName = prompt('input user name');
+checkArray(arr, userName);
+
+function fillArray(){
+	var arr = [];
+	for ( var i = 0; i < 5; i++){		
+		arr.push(prompt('input ' + (i + 1) + '-th name'));
+		// alert(arr[i]);
+	}
+	alert(arr);
+	return arr;
+}
+
+function checkArray(arr, userName){
+	var corectName = false;
+	for ( var i = 0; i < arr.length; i++){
+		if (arr[i] == userName){
+			corectName = true;
+			alert(userName + ', input success');
+			break;
+		}
+	}
+
+	if ( !corectName) {
+		alert( userName + ', input fail');
+	}
+}
